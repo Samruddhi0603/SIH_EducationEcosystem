0b686d423f6af0a6741b3b38f9e6e5cb1d4745df
Samruddhi0603 <116059940+Samruddhi0603@users.noreply.github.com>
2024-09-04 11:12:00 +0530

    Update deaf.php

--- deaf.php
+++ deaf.php
@@ -1,68 +1,84 @@
-
-<?php
-
-?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hearing impairment</title>
+    <title>Hearing Impairment</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
     <section>
-        <div class="container-fluid" style="height: 200px;background-color: blueviolet;color: white;">
-           
-           <div > <br><br><br> <h1 style="text-align: center;">
-        
-            Sign Language videos
-            </h1>
+      <div class="container-fluid" style="height: 200px; background-color: blueviolet; color: white;">
+        <div>
+          <br><br><br>
+          <h1 style="text-align: center;">
+            Sign Language Videos
+          </h1>
         </div>
-        </div>
+      </div>
     </section>
+
     <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <img src="d1.jpg" alt="" width="300px" height="250px">
-                </div>
-                <div class="col-md-4">
-                    <img src="d2.jpg" alt="" width="300px" height="250px">
-                </div>
-                <div class="col-md-4">
-                    <img src="d3.jpg" alt="" width="300px" height="250px">
-                </div>
-
-            </div>
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <iframe width="400" height="300" src="v1.mp4">
-                        </iframe>
-                    </div>
-                    <div class="col-md-6">
-                        <iframe width="400" height="300" src="v2.mp4">
-                        </iframe>
-                    </div>
-                </div>
-
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <iframe width="390" height="300" src="v3.mp4">
-                            </iframe>
-                        </div>
-                        <div class="col-md-6">
-                            <iframe width="400" height="300" src="v4.mp4">
-                            </iframe>
-                        </div>
-                    </div>
-
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="d1.jpg" alt="" width="300px" height="250px">
+          </div>
+          <div class="col-md-4">
+            <img src="d2.jpg" alt="" width="300px" height="250px">
+          </div>
+          <div class="col-md-4">
+            <img src="d3.jpg" alt="" width="300px" height="250px">
+          </div>
         </div>
+      </div>
     </section>
-    
+
+    <!-- New Section for List with Images -->
+    <section>
+      <div class="container mt-5">
+        <h2>Videos For You</h2>
+        
+        <ul class="list-unstyled">
+          <li class="mb-3">
+            <div class="row align-items-center">
+              <div class="col-md-4">
+                <img src="list-item1.jpeg" alt="" width="250px" height="200px">
+              </div>
+              <div class="col-md-8">
+                <h4>Indian Sign Language 101 | Playlist</h4>
+                <h6><a href="https://tinyurl.com/ISL101" target="_blank">Click here</a></h6>
+                <p>Welcome to Indian Sign Language 101 I, Pragya, learnt Indian Sign Language while studying BA (Hons) Multimedia and Mass Communication from IP College for Women, University of Delhi...</p>
+              </div>
+            </div>
+          </li>
+          <li class="mb-3">
+            <div class="row align-items-center">
+              <div class="col-md-4">
+                <img src="list-item3.jpeg" alt="" width="250px" height="200px">
+              </div>
+              <div class="col-md-8">
+                <h4>Alphabet (Indian Sign Language) DEAF ENABLED FOUNDATION</h4>
+                <h6><a href="https://example.com/link2" target="_blank">Click here</a></h6>
+                <p>The DEF-ISL app makes the learning of sign language easy, accessible and interesting...</p>
+              </div>
+            </div>
+          </li>
+          <li class="mb-3">
+            <div class="row align-items-center">
+              <div class="col-md-4">
+                <img src="list-item2.jpeg" alt="" width="250px" height="200px">
+              </div>
+              <div class="col-md-8">
+                <h4>Verb Signs| Indian Sign Language l ISL By PHIN Deaf</h4>
+                <h6><a href="https://goo.gl/maps/yfTCzES5AXvRCKbB9" target="_blank">Click here</a></h6>
+                <p>We are promoting ISL - Indian Sign Language and also making awareness about Sign Language to everyone...</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
     
   </body>
 </html>
